cbe37c6ce8114e35412f7f08483924fcdad1c51e
Cinea Zhan <cinea@cinea.com.cn>
2023-05-30 12:38:18 +0800

    实现了VGA打印

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@
 #![no_std] // 不链接Rust标准库
 #![no_main] // 禁用所有Rust层级的入口点
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
+use crate::vga_buffer::Writer;
 
 /// 这个函数将在panic时被调用
 #[panic_handler]
@@ -15,14 +18,8 @@ static HELLO: &[u8] = b"Every smallest dream matters.";
 
 #[no_mangle] // 不重整函数名
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
---
+++ src/vga_buffer/mod.rs
@@ -0,0 +1,163 @@
+// in src/vga_buffer/mod.rs
+
+use core::fmt;
+use core::fmt::Write;
+use volatile::Volatile;
+
+/// VGA标准颜色
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+/// 两位颜色码，在内部使用
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+/// 提交到内存中的VGA字符
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+const TAB_SIZE: usize = 4;
+
+/// VGA屏幕
+#[repr(transparent)]
+struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+/// 输出器
+pub struct Writer {
+    row_position: usize,
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            0x08 => self.backspace(), // \b
+            b'\t' => self.horizontal_tab(),
+            b'\n' => self.new_line(),
+            b'\r' => self.carriage_return(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = self.row_position.clone();
+                let col = self.column_position.clone();
+                let color_code = self.color_code.clone();
+
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                });
+
+                self.column_position += 1;
+            }
+        }
+    }
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                0x20..=0x7e | b'\n' | b'\r' | b'\t' | 0x08 => self.write_byte(byte),
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row.clone()][col].write(blank);
+        }
+    }
+
+    fn new_line(&mut self) {
+        self.row_position += 1;
+        self.column_position = 0;
+
+        if self.row_position >= BUFFER_HEIGHT {
+            // 向上滚屏
+            for row in 0..BUFFER_HEIGHT - 1 {
+                for col in 0..BUFFER_WIDTH {
+                    self.buffer.chars[row.clone()][col.clone()].write(self.buffer.chars[row.clone() + 1][col.clone()].read());
+                }
+            }
+            self.clear_row(BUFFER_HEIGHT - 1);
+        }
+    }
+
+    fn backspace(&mut self) {
+        if self.column_position > 0 {
+            self.column_position -= 1;
+        }
+    }
+
+    fn carriage_return(&mut self) {
+        self.column_position = 0;
+    }
+
+    fn horizontal_tab(&mut self) {
+        self.column_position += TAB_SIZE - (self.column_position.clone() % TAB_SIZE);
+        if self.column_position >= BUFFER_WIDTH {
+            self.new_line();
+        }
+    }
+}
+
+pub fn print_something() {
+    let mut writer = Writer {
+        row_position: 0,
+        column_position: 0,
+        color_code: ColorCode::new(Color::LightCyan, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+
+    write!(writer, "Every smallest dream matters.\n\n").unwrap();
+    write!(writer, "\t----Hello World From Cinea's Operating System\n").unwrap();
+    write!(writer, "\t\t\t\t\t\t\t\t2023.5.30").unwrap();
+}
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> Result<(), core::fmt::Error> {
+        self.write_string(s);
+        Ok(())
+    }
+}
