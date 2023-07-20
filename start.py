95cc3e30797748321a204f5ee20b479caf7f5f8c
Zhang Yao <cinea@cinea.com.cn>
2023-07-21 00:25:19 +0800

    暂存

---
+++ start.py
@@ -0,0 +1,79 @@
+import os
+import sys
+import time
+
+FS = "datadisk.img"
+FS_SOURCE = "dsk"
+
+if sys.version_info.major < 3:
+    print("Trying to call Python 3...")
+    os.system("python3 " + __file__)
+    exit(0)
+
+import shutil
+import platform
+
+
+def get_latest_modified_time(directory):
+    latest_time = 0
+
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            modified_time = os.path.getmtime(file_path)
+            if modified_time > latest_time:
+                latest_time = modified_time
+
+    return latest_time
+
+
+PWD = os.path.dirname(os.path.abspath(__file__))
+EXE_SUFFIX = ".exe" if platform.system() == "Windows" else ""
+
+argv = sys.argv
+if len(argv) < 2:
+    print("Please give me an additional argument: boot image's position.")
+    exit(1)
+
+BOOT_IMAGE = argv[1]
+
+print("Checking need for compile FS Helper...")
+FS_COMPLIER = "fs-compiler" + EXE_SUFFIX
+if not os.path.exists(FS_COMPLIER):
+    os.makedirs("../tmp-compiling", exist_ok=True)
+    TOOL_DIR = "../tmp-compiling"
+
+    if os.path.exists("../tmp-compiling"):
+        shutil.rmtree("../tmp-compiling")
+
+    if not os.path.exists("src/tools/fs-compiler"):
+        print("Cloning fs-compiler...")
+        os.system("git clone https://github.com/Cinea4678/TJU-cinea-os-tools.git ../tmp-compiling")
+    else:
+        shutil.copy("src/tools/fs-compiler", "../tmp-compiling/fs-compiler")
+
+    os.chdir(TOOL_DIR + "/fs-compiler")
+    os.system("cargo build --release")
+    os.chdir(PWD)
+    shutil.copy(TOOL_DIR + "/fs-compiler/target/release/" + FS_COMPLIER, FS_COMPLIER)
+    os.removedirs("../tmp-compiling")
+
+print("Checking need for recompile the file system...")
+re_compile = False
+if not os.path.exists(FS):
+    print("File System not exist yet...")
+    re_compile = True
+elif get_latest_modified_time(FS_SOURCE) > os.path.getmtime(FS):
+    print("File System is older than source dir...")
+    re_compile = True
+if re_compile:
+    print("Recompiling the file system...")
+    os.system(FS_COMPLIER + " " + FS_SOURCE + " " + FS)
+    os.system("qemu-img create -f qcow2 -o preallocation=off datadisk.qcow2 4G")
+    os.system("qemu-img convert -O qcow2 " + FS + " datadisk.qcow2")
+else:
+    print("File System is already newest.")
+
+print("Starting QEMU...")
+# os.system(f"qemu-system-x86_64 -drive format=raw,file={BOOT_IMAGE} -serial " +
+#          "stdio -m 1G -monitor telnet:localhost:4444,server,nowait -drive format=qcow2,file=datadisk.qcow2")
